635ba52b3e866fb9ab37643d930eede03f9ee3da
Ruka-Chan <141768871+codex0555@users.noreply.github.com>
2024-01-29 11:17:12 +0530

    v1.0

--- src/routes/episode.js
+++ src/routes/episode.js
@@ -2,15 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-//https://aniwatchtv.to/ajax/v2/episode/sources?id=1081246
-//https://aniwatchtv.to/ajax/v2/episode/servers?episodeId=114910
-//https://aniwatchtv.to/ajax/v2/episode/list/18964
-//https://aniwatchtv.to/ajax//schedule/list?tzOffset=-330&date=2024-01-27
-
 const episode = express();
+const cors = require('cors');
 
 const USER_AGENT ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
 const ACCEPT_ENCODING_HEADER = "gzip, deflate, br";
+
+episode.use(cors());
 
 episode.get('/episode/:id', async ( req, res)=>{
     const episodeanime = req.params.id.match(/\d+/);
