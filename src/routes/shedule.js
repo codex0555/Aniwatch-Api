341378180b5d68fba02d45fe2541a787ccb74846
Ruka-Chan <141768871+codex0555@users.noreply.github.com>
2024-01-29 11:20:50 +0530

    cors

--- src/routes/shedule.js
+++ src/routes/shedule.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const shedule = express();
+const cors = require('cors');
 
 const ACCEPT_ENCODING_HEADER = "gzip, deflate, br";
 const USER_AGENT ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
 const ACCEPT_HEADER =
   "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+
+shedule.use(cors());
 
 
 shedule.get('/shedule/:id', async function( request, response){
