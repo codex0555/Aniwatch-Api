e168ca39c00422ddc7b99e8cf85cfc92e0cbd9fe
Ruka-Chan <141768871+codex0555@users.noreply.github.com>
2024-01-29 11:18:41 +0530

    cors

--- src/routes/mix.js
+++ src/routes/mix.js
@@ -3,8 +3,11 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const mix = express();
+const cors = require('cors');
 
 const USER_AGENT ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
+
+mix.use(cors());
 
 
 
