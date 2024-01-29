57d3ed8157e41160eb89d0a3374bdf79ff0afcab
Ruka-Chan <141768871+codex0555@users.noreply.github.com>
2024-01-29 11:16:22 +0530

    cors

--- src/routes/app.js
+++ src/routes/app.js
@@ -4,9 +4,11 @@ const cheerio = require('cheerio');
 
 const app = express();
 // const port = 3000;
+const cors = require('cors');
 
 const USER_AGENT ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
 
+app.use(cors());
 
 
 // Route to parse information from a website
